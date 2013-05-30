f2c5751ce9a349b78a9b07453b8cff3b6ab9894b
Greg Davidson <davidson.mj@gmail.com>
2013-05-30 13:03:15 -0400

    initial image processing

--- drone.js
+++ drone.js
@@ -1,13 +1,39 @@
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
+var cv = require("opencv");
+var quad = require("./quad"); 
+var fs = require("fs");
+client.config('video:video_channel', 3);
+require('ar-drone-png-stream')(client, { port: 8000 });
+
 
 client.takeoff();
 
-client
-  .after(10000, function() {
-    this.stop();
-    this.land();
-  });
+client.after(5000, function(){
+  client.up(1);
+})
+.after(2000, function() {
+  this.stop(); 
+});
 
-var pngStream = client.getPngStream();
-pngStream.on('data', console.log);
+
+var s = new cv.ImageStream();
+
+//client.takeoff();
+//
+//client
+//  .after(5000, function() {
+//    this.stop();
+//    this.land();
+//  });
+
+s.on('data', function(matrix){
+  console.log(quad(matrix));
+})
+
+client.getPngStream().pipe(s);
+
+client.after(10000, function() {
+  this.land();
+});
+  
